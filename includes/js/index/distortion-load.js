35d4cd2fca53a501d546f2760d9feb8498328cd4
Eric Bellamy <eric@bellamy.cc>
2024-06-22 02:34:24 -0400

    finished & optimized portfolio

--- includes/js/index/distortion-load.js
+++ includes/js/index/distortion-load.js
@@ -1,31 +1,21 @@
 window.loadDistortion = function (initCallback) {
-	const img = document.getElementById("displacementMap");
-	img.addEventListener("load", function () {
+	const image = document.createElement("img");
+	image.addEventListener("load", function () {
 		setTimeout(function () {
 			window.distortionScroller = new WebGLDistortionScroller({
-				displacement: document.getElementById("displacementMap"),
+				displacement: image,
 				afterInit: initCallback,
-				// afterInit: function () {
-
-
-				// 	// switch (window.distortionScroller.mode) {
-				// 	// 	case 0:
-				// 	// 		document.body.style.background = `#000`;
-				// 	// 		break;
-				// 	// 	case 1:
-				// 	// 		document.body.style.background = `#fff`;
-				// 	// 		break;
-				// 	// }
-
-				// 	// window.distortionScroller.start();
-
-				// 	// // This is the scroll function that determines what is happening on the page based on what section we're in
-				// 	// // highPerformanceScroll();
-				// },
 				mode: [0, [1, 1, 1], 0, 0.93, 0.95]
 			});
 			window.distortionScroller.init();
 		}, 100);
 	});
-	img.src = img.dataset.src;
+	// image.src = "https://use.performanceprojects.site/website/portfolio/jpg/webgl/distortion.jpg";
+	image.setAttribute("alt", "webgl distortion noise map");
+	image.width = 128;
+	image.height = 128;
+	image.src = "/exports/img/distortion.jpg";
+	image.style.opacity = 0;
+	image.style.zIndex = -100;
+	document.body.appendChild(image);
 }
